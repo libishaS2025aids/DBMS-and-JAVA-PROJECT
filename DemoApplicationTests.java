739bcbaee66634321f12e67aedd555c4fc9a90d3
libishaS2025aids <libisha.s2025aids@sece.ac.in>
2026-09-17 08:55:00 +0530

    Add files via upload

---
+++ DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
